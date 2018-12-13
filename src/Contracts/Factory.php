299b615926b7f332d929f56af1260eb902f73602
Claudin <c.daniel@amigoexpress.com>
2018-12-12 23:29:01 -0500

    Added contract and facade

---
+++ src/Contracts/Factory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Awobaz\Mutator\Contracts;
+
+use Closure;
+
+interface Factory
+{
+    /**
+     * Register a custom mutator extension.
+     *
+     * @param  string  $mutator
+     * @param  \Closure|string  $extension
+     * @return void
+     */
+    public function extend($mutator, Closure $extension);
+}
---
+++ src/Facades/Mutator.php
@@ -0,0 +1,24 @@
+<?php namespace Awobaz\Mutator\Facades;
+
+/**
+ * This file is part of Mutator,
+ * eloquent getters/setters solution for Laravel 5's Eloquent.
+ *
+ * @license MIT
+ * @package Awobaz\Mutators
+ */
+
+use Illuminate\Support\Facades\Facade;
+
+class Mutator extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'mutator';
+    }
+}
